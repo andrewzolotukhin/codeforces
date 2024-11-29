eda6a9d4aa17ebca1f410dc7ccb2b2e2ca2dae2f
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2024-11-29 10:43:19 +0200

    P977A: accepted

---
+++ 977/A/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	int n, k;
+	std::cin >> n >> k;
+	while (k--)
+	{
+		if (n % 10 == 0)
+		{
+			n /= 10;
+		}
+		else
+		{
+			n--;
+		}
+	}
+	std::cout << n << std::endl;
+}

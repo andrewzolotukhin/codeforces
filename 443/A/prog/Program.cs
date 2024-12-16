cd8c68c8151199096d32a517939cb4db8bd3feef
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2024-12-16 05:40:23 +0200

    P443A: accepted

---
+++ 443/A/prog/Program.cs
@@ -0,0 +1,15 @@
+﻿var s = Console.ReadLine();
+
+s = s.Replace("{", "").Replace("}", "").Replace(" ", "");
+
+var arr = s.Split(",", StringSplitOptions.RemoveEmptyEntries);
+var d = new Dictionary<char, int>();
+
+foreach (var ch in arr)
+{
+  if(!d.ContainsKey(ch[0])) {
+    d.Add(ch[0], 1);
+  }
+}
+
+Console.WriteLine(d.Keys.Count);

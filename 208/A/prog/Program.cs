2550655e6690e74d87f8915ad04ee2ede5ade99f
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2024-12-18 15:18:14 +0200

    P208A: accepted

---
+++ 208/A/prog/Program.cs
@@ -0,0 +1,15 @@
+﻿var s = Console.ReadLine();
+
+const string dup = "WUB";
+
+while(s.StartsWith(dup)) {
+  s = s.Substring(dup.Length);
+}
+
+s = string.Join(' ', s.Split(dup, StringSplitOptions.RemoveEmptyEntries));
+
+while(s.EndsWith(dup)) {
+  s = s.Substring(0, s.Length - dup.Length);
+}
+
+Console.WriteLine(s);

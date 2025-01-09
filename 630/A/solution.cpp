033e910d42c429b1bfdcf82229bc81cd896965b3
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-01-09 21:01:52 +0200

    P630A: accepted

---
+++ 630/A/solution.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+
+  long long n;
+  cin >> n;
+  cout << 25 << '\n';
+}

9b55519b9522784914ed02ef051a3727389fbc50
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-01-23 02:04:57 +0200

    P579A: accepted

---
+++ 579/A/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+
+  int n;
+  cin >> n;
+
+  cout << __builtin_popcount(n) << '\n';
+}

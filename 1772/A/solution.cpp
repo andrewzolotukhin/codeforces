9f78be8e38913b1f292f15c4cc5d4df1c6e05998
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-03-20 04:01:01 +0200

    P1772: accepted

---
+++ 1772/A/solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {}
+
+int main() {
+  int t;
+  scanf("%d", &t);
+
+  while (t--) {
+    int a, b;
+
+    scanf("%d+%d", &a, &b);
+
+    printf("%d\n", a + b);
+  }
+}

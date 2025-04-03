940d390a069386feb6dd5e38769a19f90851c7d7
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-04-04 02:20:12 +0300

    P959A: accepted

---
+++ 959/A/solution.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+
+  int n;
+  cin >> n;
+  cout << ((n % 2) ? "Ehab\n" : "Mahmoud\n");
+}

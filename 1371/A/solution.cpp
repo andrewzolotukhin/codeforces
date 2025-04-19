170265c390f122f618599802de9f75d5102da8d0
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-04-19 05:15:00 +0300

    P1371A: accepted

---
+++ 1371/A/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int x;
+    cin >> x;
+    cout << ((x + 1) / 2) << '\n';
+  }
+}

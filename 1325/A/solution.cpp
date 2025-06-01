f6ce4f36265b2d904c9f068ee7e3f749a05108d9
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-06-01 04:37:32 +0300

    P1325A: accepted

---
+++ 1325/A/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int x;
+    cin >> x;
+
+    cout << 1 << ' ' << (x - 1) << '\n';
+  }
+}

4b09e90743d8dac39c988092c17eeb504f9267e9
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-07-23 05:49:10 +0300

    P2125B: accepted

---
+++ 2125/B/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long solve() {
+  long long a, b, k;
+  cin >> a >> b >> k;
+
+  int g = gcd(a, b);
+  a = a / g;
+  b = b / g;
+
+  if (a <= k && b <= k) {
+    return 1;
+  }
+
+  return 2;
+}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    cout << solve() << '\n';
+  }
+}

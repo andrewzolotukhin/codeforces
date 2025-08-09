dcd23cbb0bd7d6526b044e01bdb2dccf2c9f55e9
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-08-10 01:13:54 +0300

    P1337A : accepted

---
+++ 1337/A/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+
+  cout << b << ' ' << c << ' ' << c << '\n';
+}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}

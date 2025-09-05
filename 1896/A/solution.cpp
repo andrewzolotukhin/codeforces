d791a78e471b052ef8f74e9adc28723c59bf4955
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-09-05 06:16:33 +0300

    P1896A: accepted

---
+++ 1896/A/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (auto &x : a)
+    cin >> x;
+
+  if (a[0] == 1) {
+    cout << "YES\n";
+  } else {
+    cout << "NO\n";
+  }
+}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+
+  int t;
+  cin >> t;
+  while (t--)
+    solve();
+}

960264ecb1ff68edcc61cd51046f0cc9b4c149d7
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-09-06 04:13:28 +0300

    P1186A: accepted

---
+++ 1186/A/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int n, m, k;
+  cin >> n >> m >> k;
+  if (m < n || k < n) {
+    cout << "No\n";
+    return;
+  }
+
+  cout << "Yes\n";
+}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+
+  solve();
+}

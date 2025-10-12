5de87e7e676552f4d37e6d2320dea9ccf6cc270d
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-10-12 05:32:06 +0300

    P2155A: accepted

---
+++ 2155/A/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int n;
+  cin >> n;
+  cout << 2 * (n - 1) << '\n';
+}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+
+  int n;
+  cin >> n;
+  while (n--)
+    solve();
+}

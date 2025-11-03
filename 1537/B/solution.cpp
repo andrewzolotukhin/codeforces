2ce87f08e058af0cd8589aa38fcc9a192e46ab73
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-11-03 03:57:27 +0200

    P1537B: accepted

---
+++ 1537/B/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int n, m, i, j;
+  cin >> n >> m >> i >> j;
+  cout << 1 << ' ' << 1 << ' ' << n << ' ' << m << '\n';
+}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+
+  int t;
+  cin >> t;
+  while (t--)
+    solve();
+}
